2ee7f8a5743384cc6d9097cd0488521932dd79c3
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2022-09-23 16:46:16 +0300

    added config paramter to website build

---
+++ docs/website/.vuepress/.cache/deps/@vue_devtools-api.js
@@ -0,0 +1,11 @@
+import {
+  isPerformanceSupported,
+  now,
+  setupDevtoolsPlugin
+} from "./chunk-5JFDNZE2.js";
+export {
+  isPerformanceSupported,
+  now,
+  setupDevtoolsPlugin
+};
+//# sourceMappingURL=@vue_devtools-api.js.map
